8a1c1e3e7d5bd80339e1e9635ceac332a3fdb505
Anthony69600 <a.verchere69@gmail.com>
2021-10-30 16:23:02 +0200

    commit_twig1

--- public/products.php
+++ public/products.php
@@ -1,12 +1,6 @@
+
 <?php
-
-// Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
-
-
-//$name = 'Wilder';
-
-//echo $twig->render('home.html.twig', ['name' => $name]);
 
 $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
 
